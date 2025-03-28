57a16f53601e1e8fa398098bcd8a9b25c1aef472
Felipe Matos <flpmatosa@gmail.com>
2025-03-28 09:15:31 -0300

    feat: add echo tool

---
+++ HelloMcp.Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using ModelContextProtocol;
+using ModelContextProtocol.Server;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel;
+
+Console.WriteLine("Running...");
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"hello {message}";
+}
